ba4e025224f34b90012447578c4c899f44a2a421
bates phillip <58259phil@gmail.com>
2026-06-21 17:55:05 +1000

    add microsoft clarity

--- src/app/layout.js
+++ src/app/layout.js
@@ -46,6 +46,9 @@ export default function RootLayout({ children }) {
     <html lang="en" className={`${playfair.variable} ${dmSans.variable}`}>
       <head>
         <meta name="agd-partner-manual-verification" />
+        <Script id="clarity-script" strategy="afterInteractive">
+         {`(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window, document, "clarity", "script", "xae336qkat");`}
+        </Script>
       </head>
       <body className="font-body antialiased">
         {/* Google Analytics */}
